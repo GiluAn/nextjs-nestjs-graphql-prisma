3c5413bdbb746551cb9f68670a29c5e002e75c56
Gilu Ahn <gilu9111@gmail.com>
2022-12-03 20:24:49 +0900

    backend graphql prisma settings

---
+++ backend/prisma/sql/init.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
